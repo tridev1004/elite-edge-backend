763848aea1047cf3bfc1a5910288d80ddda0ecfb
tridev1004 <85545474+tridev1004@users.noreply.github.com>
2025-01-12 23:17:22 +0530

    fix

--- middlewares/validations/registerValidation.js
+++ middlewares/validations/registerValidation.js
@@ -4,7 +4,6 @@ module.exports = [
     body("fullName").isString().withMessage('fullname must be string'),
     body("password").isStrongPassword().withMessage(`Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one symbol.`),
     body("email").isEmail().withMessage('email must be Invalid Format'),
-    body('phone').optional().isMobilePhone('ar-EG').withMessage('Please enter a valid Egyptian phone number.'),
     body("address").optional().isObject().withMessage('address must be object'),
     body('address.city').optional().isString().withMessage('city must be string'),
     body('address.street').optional().isString().withMessage('street must be string'),
