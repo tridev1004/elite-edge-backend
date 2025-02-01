81b30eab4ba5f6e202ef12d0720c1648d7efd4ff
tridev1004 <85545474+tridev1004@users.noreply.github.com>
2025-02-01 09:31:24 +0530

    fix

--- utils/cloudinary.config.js
+++ utils/cloudinary.config.js
@@ -3,6 +3,9 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
+
+
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
