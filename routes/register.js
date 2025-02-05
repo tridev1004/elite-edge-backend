243d65d66ccf047cdb02f0a9d75f0e588a7bef97
tridev1004 <85545474+tridev1004@users.noreply.github.com>
2025-02-05 08:58:39 +0530

    fix

--- routes/register.js
+++ routes/register.js
@@ -3,7 +3,6 @@ const express = require("express");
 const { register } = require("../controllers/registerController.js");
 const validation = require("../middlewares/validations/registerValidation");
 const validator = require("../middlewares/validations/validator");
-console.log("register route");
 const router = express.Router();
 router.route("/register").post(validation, validator, register);
 
